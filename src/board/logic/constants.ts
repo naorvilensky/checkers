4f03880afc09aeb107c764273626bb8322781e14
naorvilensky <naor.vilensky>
2022-12-10 01:15:46 +0200

    fix board sizing

--- src/board/logic/constants.ts
+++ src/board/logic/constants.ts
@@ -1,9 +1,15 @@
 export const NUMBER_OF_CELLS = 8;
-export const BOARD_SIZE = Math.min(window.screen.width, window.screen.height);
-export const CELL_SIZE = Math.floor(BOARD_SIZE / NUMBER_OF_CELLS);
+export const BOARD_SIZE = calculateBoardSize();
+export const CELL_SIZE = BOARD_SIZE / NUMBER_OF_CELLS;
 export const BOARD_SIZE_ADDITION = CELL_SIZE === BOARD_SIZE / NUMBER_OF_CELLS ? 2 : 0;
 
 export interface CellCoordinates {
     i: number;
     j: number;
+}
+
+function calculateBoardSize(): number {
+    let size = Math.min(window.screen.width, window.screen.height);
+
+    return size - (size % 8);
 }
